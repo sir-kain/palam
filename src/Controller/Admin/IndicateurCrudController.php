af25d84e120201b1f9224b38f6375183c4aba9f5
Waly <ahmadouwalyndiaye@gmail.com>
2022-09-22 21:16:49 +0000

    Handle export excel file for indicateur

--- src/Controller/Admin/IndicateurCrudController.php
+++ src/Controller/Admin/IndicateurCrudController.php
@@ -3,11 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Indicateur;
+use App\Repository\IndicateurRepository;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\ORM\QueryBuilder as ORMQueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\BatchActionDto;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
@@ -34,9 +38,59 @@ class IndicateurCrudController extends AbstractCrudController
         ];
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        $exportCSV = Action::new("exportCSV", 'Export excel')
+            ->setIcon('fas fa-download')
+            ->linkToCrudAction('downloadCSV');
+        return $actions->addBatchAction($exportCSV);
+    }
+
+    public function downloadCSV(BatchActionDto $batchActionDto)
+    {
+        $className = $batchActionDto->getEntityFqcn();
+        $entityManager = $this->container->get('doctrine')->getRepository($className);
+        assert($entityManager instanceof IndicateurRepository);
+
+        // Excel file name for download 
+        $fileName = "indicateurs_" . date('Y-m-d') . ".xls";
+
+        // Column names 
+        $fields = ['No', 'INDICATEURS', 'Périodicité de collecte des données', 'Source des données', 'Analyse et interprétation des données'];
+
+        // Display column names as first row 
+        $excelData = implode("\t", array_values($fields)) . "\n";
+
+        // Fetch records from database 
+        foreach ($entityManager->getOrderedList() as $row) {
+            $line = [$row['code'], $row['libelle'], $row['period'], $row['source'], $row['analyse']];
+            array_walk($line, function ($row) {
+                return $this->filterData($row);
+            });
+            $excelData .= implode("\t", array_values($line)) . "\n";
+        }
+
+        // Headers for download 
+        header("Content-Type: application/vnd.ms-excel");
+        header("Content-Disposition: attachment; filename=\"$fileName\"");
+
+        // Render excel data 
+        echo $excelData;
+
+        exit;
+    }
+
     public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): ORMQueryBuilder
     {
         $queryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
         return $queryBuilder->orderBy('entity.type_indicateur');
+    }
+
+    // Filter the excel data 
+    private function filterData(&$str)
+    {
+        $str = preg_replace("/\t/", "\\t", $str);
+        $str = preg_replace("/\r?\n/", "\\n", $str);
+        if (strstr($str, '"')) $str = '"' . str_replace('"', '""', $str) . '"';
     }
 }
--- src/Repository/IndicateurRepository.php
+++ src/Repository/IndicateurRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Indicateur;
+use App\Entity\TypeIndicateur;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,7 +17,7 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class IndicateurRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private TypeIndicateurRepository $typeIndicateurRepository)
     {
         parent::__construct($registry, Indicateur::class);
     }
@@ -39,28 +40,59 @@ class IndicateurRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return Indicateur[] Returns an array of Indicateur objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('i')
-//            ->andWhere('i.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('i.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return Indicateur[] Returns an array of Indicateur objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('i')
+    //            ->andWhere('i.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('i.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?Indicateur
-//    {
-//        return $this->createQueryBuilder('i')
-//            ->andWhere('i.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?Indicateur
+    //    {
+    //        return $this->createQueryBuilder('i')
+    //            ->andWhere('i.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+
+    public function getOrderedList()
+    {
+        $list = [];
+        foreach ((array) $this->typeIndicateurRepository->findAll() as $typeIndicateur) {
+            assert($typeIndicateur instanceof TypeIndicateur);
+            array_push(
+                $list,
+                [
+                    'code' => $typeIndicateur->getLibelle(),
+                    'libelle' => '',
+                    'period' => '',
+                    'source' => '',
+                    'analyse' => '',
+                ]
+            );
+
+
+            foreach ($typeIndicateur->getIndicateurs()->toArray() as $indicateur) {
+                assert($indicateur instanceof Indicateur);
+                array_push($list, [
+                    'code' => $indicateur->getCode(),
+                    'libelle' => $indicateur->getLibelle(),
+                    'period' => $indicateur->getPeriodicite()->getLibelle(),
+                    'source' => $indicateur->getSource(),
+                    'analyse' => $indicateur->getAnalyseInterpretation(),
+                ]);
+            }
+        }
+        return $list;
+    }
 }
