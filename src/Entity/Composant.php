1fc36c11f7bf1d1d265c9b5080f813effaef1555
Waly <ahmadouwalyndiaye@gmail.com>
2022-09-28 10:42:51 +0000

    Add assertion on composant libelle and form_errors widget

--- src/Entity/Composant.php
+++ src/Entity/Composant.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ComposantRepository::class)]
 class Composant
@@ -17,6 +18,7 @@ class Composant
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $libelle = null;
 
     #[ORM\OneToMany(mappedBy: 'composant', targetEntity: Activite::class)]
