253b43901d38e4130d93166f4fd1f8ba1edf2df6
Waly <ahmadouwalyndiaye@gmail.com>
2022-09-28 16:18:49 +0000

    Handle user form

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -12,6 +13,15 @@ class UserCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return User::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->overrideTemplates([
+                'crud/edit' => 'admin/user/edit.html.twig',
+                'crud/new' => 'admin/user/new.html.twig',
+            ]);
     }
 
     public function configureFields(string $pageName): iterable
